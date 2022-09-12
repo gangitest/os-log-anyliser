6cd55cda14666e897ae14591070136c96b02ae89
gangi reddy <nallagondu@hotmail.com>
2022-09-12 17:11:00 +0000

    Os log anyliser

--- main.py
+++ main.py
@@ -1,64 +1,57 @@
 import os
+import sys
 
-cwd = os.getcwd()
+sys.path.insert(0, os.path.abspath('../..'))
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'oslo.sphinx'
+  ]
 
-print("current directory:",cwd)
+# autodoc generation is a bit aggressive and a nuisance when doing heavy
+# text edit cycles.
+# execute "export SPHINX_DEBUG=1" in your terminal to disable
 
-lsdata = os.getcwdb()
+# The suffix of source filenames.
+source_suffix = '.rst'
 
-print(lsdata)
+# The master toctree document.
+master_doc = 'index'
 
-# Function to Get the current  
-# working directory 
-def current_path(): 
-    print("Current working directory before") 
-    print(os.getcwd()) 
-    print() 
-    
-    
-# Driver's code 
-# Printing CWD before 
-current_path() 
-    
-# Changing the CWD 
-os.chdir('../') 
-    
-# Printing CWD after 
-current_path() 
-#make directory in Current path 
-'''ath = os.path.join(test1) 
-os.mkdir(path)'''
+# General information about the project.
+project = u'replace with the name for the git repo'
+copyright = u'Test Logs anyliser'
 
-# Directory 
-#directory = "test1"
-  
-# Parent Directory path 
-parent_dir = "c:/users/nallag/"
-  
-# Path 
-path = os.path.join(parent_dir, directory) 
-  
-# Create the directory 
-# 'GeeksForGeeks' in 
-# '/home / User / Documents' 
-os.mkdir(path) 
-print("Directory '% s' created" % directory) 
-  
-# Directory 
-directory = "Geeks"
-  
-# Parent Directory path 
-parent_dir = "c:/users/nallag/"
-  
-# mode 
-mode = 0o666
-  
-# Path 
-path = os.path.join(parent_dir, directory) 
-  
-# Create the directory 
-# 'GeeksForGeeks' in 
-# '/home / User / Documents' 
-# with mode 0o666 
-os.mkdir(path, mode) 
-print("Directory '% s' created" % directory) 
+# If true, '()' will be appended to :func: etc. cross-reference text.
+add_function_parentheses = True
+
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+add_module_names = True
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# -- Options for HTML output --------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  Major themes that come with
+# Sphinx are currently 'default' and 'sphinxdoc'.
+# html_theme_path = ["."]
+# html_theme = '_theme'
+# html_static_path = ['static']
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = '%sdoc' % project
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, documentclass
+# [howto/manual]).
+latex_documents = [
+    ('index',
+     '%s.tex' % project,
+     u'%s Documentation' % project,
+     u'OpenStack Foundation', 'manual'),
+]
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {'http://docs.python.org/': None}
